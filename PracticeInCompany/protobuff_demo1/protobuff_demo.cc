b1309673cc74b8e147d87fb4c35afcbb19cfe3e7
[weiweikaikai] <weeks@debian>
2017-10-24 17:07:12 +0800

    add someting

--- PracticeInCompany/protobuff_demo1/protobuff_demo.cc
+++ PracticeInCompany/protobuff_demo1/protobuff_demo.cc
@@ -7,11 +7,21 @@ using namespace std;
 int main(int argc,char *argv[])
 {
     demo::msg writer;
-    writer.set_msgtype(1);
-    writer.set_msginfo("i am weeks");
-    writer.set_msgfrom("127.0.0.1");
+   
+    writer.set_int_info(1);
+    writer.set_string_info("i am wk");
 
-     //C++string序列化和序列化API   
+    demo::data_t* data1 = writer.mutable_data_info();
+    data1->set_data_int_info(2);
+    data1->set_name("wk");
+
+    for (int i=0;i<5;++i) {
+        demo::kv_t* data2 = writer.add_vector_info();
+        data2->set_key(i);
+        data2->set_value("hello");
+    }
+    
+    //C++string序列化和序列化API   
    
     //客户端格式化信息
     string in_data;
@@ -21,16 +31,25 @@ int main(int argc,char *argv[])
     //客户端将数据放到网络上   
     string tmp=in_data;
 
-
-
-    //服务端从网络上拿数据 
+    //模拟服务端从网络上拿数据 
     string out_data=tmp;
     //服务端反格式化信息
     demo::msg reader;
     reader.ParseFromString(out_data);
-    cout<<"msg_type: "<<reader.msgtype()<<endl;
-    cout<<"msg_info: "<<reader.msginfo()<<endl;
-    cout<<"msg_from: "<<reader.msgfrom()<<endl;
+    cout<<"int_info: "<<reader.int_info()<<endl;
+    cout<<"string_info: "<<reader.string_info()<<endl;
+    demo::data_t * data3 = reader.mutable_data_info();
+    cout<<"data_t {\n";
+        cout<<"data_int_info: "<<data3->data_int_info()<<endl;
+        cout<<"name: "<<data3->name()<<endl;
+    cout<<"}\n";
 
+    for (int i=0;i<reader.vector_info_size();++i) {
+     cout<<"kv_t {\n";
+       const demo::kv_t &data4 = reader.vector_info(i);
+        cout<<"key: "<<data4.key()<<endl;
+        cout<<"value: "<<data4.value()<<endl;
+     cout<<"}\n";
+     }
     return 0;
 }
--- PracticeInCompany/protobuff_demo1/start.sh
+++ PracticeInCompany/protobuff_demo1/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
     protoc --cpp_out=. msg.proto    #生成对应的msg.pb.h和msg.pb.cpp文件
     g++ protobuff_demo.cc msg.pb.cc -o protobuff_test  -lprotobuf
+
+    ./protobuff_test 
+    rm -rf ./protobuff_test msg.pb.cc msg.pb.h
